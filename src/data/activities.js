436f8d6b0d21ee66a8f44f6cc6fcd0031a7bc3aa
SimonTech <contato.simon.tech@gmail.com>
2026-06-22 14:43:34 -0300

    add activities data for healthcare and pharmaceutical services

---
+++ src/data/activities.js
@@ -0,0 +1,38 @@
+const activities = [
+  {
+    cnae: "8610-1/01",
+    descricao: "Atividades de atendimento hospitalar, exceto pronto-socorro e unidades para atendimento a urgências",
+    grupo: "Serviços de Saúde",
+    nivelRisco: 3, 
+    documentosNecessarios: [1, 2, 3, 4, 5, 6, 7, 8, 21, 23, 27, 29, 32, 33, 38, 40, 41, 45, 52] 
+  },
+  {
+    cnae: "8610-1/02",
+    descricao: "Atividades de atendimento em pronto-socorro e unidades para atendimento a urgências",
+    grupo: "Serviços de Saúde",
+    nivelRisco: 3,
+    documentosNecessarios: [1, 2, 3, 4, 5, 6, 21, 23, 29, 32, 40, 41, 52]
+  },
+  {
+    cnae: "8640-2/05",
+    descricao: "Serviços de diagnóstico por imagem com uso de radiação ionizante, exceto tomografia computadorizada",
+    grupo: "Serviços de Saúde / Radiação",
+    nivelRisco: 3,
+    documentosNecessarios: [1, 2, 3, 20, 23, 25, 28, 29, 32, 37, 40, 42, 47, 48, 52, 61] 
+  },
+  {
+    cnae: "2121-1/01",
+    descricao: "Fabricação de medicamentos alopáticos para uso humano",
+    grupo: "Indústria de Produtos de Interesse da Saúde",
+    nivelRisco: 3,
+    documentosNecessarios: [1, 2, 3, 4, 5, 6, 7, 8, 23, 29, 32, 39, 40, 43, 52, 53, 56, 58]
+  },
+    {
+    cnae: "2121-1/02",
+    descricao: "Fabricação de medicamentos homeopáticos para uso humano",
+    grupo: "Indústria de Produtos de Interesse da Saúde",
+    nivelRisco: 3,
+    documentosNecessarios: [1, 2, 3, 4, 5, 6, 7, 8, 23, 29, 32, 39, 40, 43, 52, 53, 56, 58]
+    }
+    //todo
+];
